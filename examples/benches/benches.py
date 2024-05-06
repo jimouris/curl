e3bebe8ff0bd6ef2099fd4fe2dbcc88f028b5357
Dimitris Mouris <dimitris@nillion.com>
2024-05-06 17:50:34 -0400

    feat: Add primitive ops benchmakring

---
+++ examples/benches/benches.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+
+import logging
+import timeit
+from collections import namedtuple
+import numpy as np
+import pandas as pd
+import torch
+import functools
+
+import crypten
+import crypten.communicator as comm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+
+
+Runtime = namedtuple("Runtime", "mid q1 q3")
+
+
+def time_me(func=None, n_loops=10):
+    """Decorator returning average runtime in seconds over n_loops
+
+    Args:
+        func (function): invoked with given args / kwargs
+        n_loops (int): number of times to invoke function for timing
+
+    Returns: tuple of (time in seconds, inner quartile range, function return value).
+    """
+    if func is None:
+        return functools.partial(time_me, n_loops=n_loops)
+
+    @functools.wraps(func)
+    def timing_wrapper(*args, **kwargs):
+        return_val = func(*args, **kwargs)
+        times = []
+        for _ in range(n_loops):
+            start = timeit.default_timer()
+            func(*args, **kwargs)
+            times.append(timeit.default_timer() - start)
+        mid_runtime = np.quantile(times, 0.5)
+        q1_runtime = np.quantile(times, 0.25)
+        q3_runtime = np.quantile(times, 0.75)
+        runtime = Runtime(mid_runtime, q1_runtime, q3_runtime)
+        return runtime, return_val
+
+    return timing_wrapper
+
+
+class FuncBenchmarks:
+    """Benchmarks runtime and error of crypten functions against PyTorch
+
+    Args:
+        tensor_size (int or tuple): size of tensor for benchmarking runtimes
+    """
+
+    UNARY = [
+        "sigmoid",
+        "relu",
+        "tanh",
+        "exp",
+        "log",
+        "reciprocal",
+        "cos",
+        "sin",
+        "sum",
+        "mean",
+        "neg",
+        "erf"
+    ]
+
+    DOMAIN = torch.arange(start=1.01, end=10, step=0.01)
+    # for exponential, sin, and cos
+    TRUNCATED_DOMAIN = torch.arange(start=0.001, end=10, step=0.001)
+
+    def __init__(self, tensor_size, device="cpu"):
+        self.device = torch.device(device)
+        self.tensor_size = tensor_size
+
+        # dataframe for benchmarks
+        self.df = None
+
+    def __repr__(self):
+        if self.df is not None:
+            return self.df.to_string(index=False, justify="left")
+        return "No Function Benchmarks"
+
+    @staticmethod
+    @time_me
+    def time_func(x, func, y=None):
+        """Invokes func as a method of x"""
+        if y is None:
+            return getattr(x, func)()
+
+        return getattr(x, func)(y)
+
+    def get_runtimes(self):
+        """Returns plain text and crypten runtimes"""
+        x = torch.rand(self.tensor_size, device=self.device)*5 + 1
+        x_enc = crypten.cryptensor(x)
+
+        runtimes, runtimes_enc = [], []
+
+        for func in FuncBenchmarks.UNARY:
+            runtime, _ = FuncBenchmarks.time_func(x, func)
+            runtimes.append(runtime)
+
+            runtime_enc, _ = FuncBenchmarks.time_func(x_enc, func)
+            runtimes_enc.append(runtime_enc)
+
+        return runtimes, runtimes_enc
+
+    @staticmethod
+    def calc_abs_error(ref, out):
+        """Computes total absolute error"""
+        ref, out = ref.cpu(), out.cpu()
+        if ref.dtype == torch.bool:
+            errors = (out != ref).numpy().sum()
+            return errors
+        errors = torch.abs(out - ref).numpy()
+        return errors.sum()
+
+    @staticmethod
+    def calc_relative_error(ref, out):
+        """Computes average relative error"""
+        ref, out = ref.cpu(), out.cpu()
+        if ref.dtype == torch.bool:
+            errors = (out != ref).numpy().sum() // ref.nelement()
+            return errors
+        errors = torch.abs((out - ref) / ref)
+        # remove inf due to division by tiny numbers
+        errors = errors[errors != float("inf")].numpy()
+        return errors.mean()
+
+    def call_function_on_domain(self, func):
+        """Call plain text and CrypTen function on given function
+        Uses DOMAIN or TRUNCATED_DOMAIN
+
+        Returns: tuple of (plain text result, encrypted result)
+        """
+        DOMAIN, TRUNCATED_DOMAIN = (
+            FuncBenchmarks.DOMAIN,
+            FuncBenchmarks.TRUNCATED_DOMAIN,
+        )
+        if hasattr(DOMAIN, "to") and hasattr(TRUNCATED_DOMAIN, "to"):
+            DOMAIN, TRUNCATED_DOMAIN = (
+                DOMAIN.to(device=self.device),
+                TRUNCATED_DOMAIN.to(device=self.device),
+            )
+        y = torch.rand(DOMAIN.shape, device=self.device)
+        DOMAIN_enc, _ = crypten.cryptensor(DOMAIN), crypten.cryptensor(y)
+        TRUNCATED_DOMAIN_enc = crypten.cryptensor(TRUNCATED_DOMAIN)
+
+        if func in ["exp", "cos", "sin"]:
+            ref, out_enc = (
+                getattr(TRUNCATED_DOMAIN, func)(),
+                getattr(TRUNCATED_DOMAIN_enc, func)(),
+            )
+        elif func in FuncBenchmarks.UNARY:
+            ref, out_enc = getattr(DOMAIN, func)(), getattr(DOMAIN_enc, func)()
+        else:
+            raise ValueError(f"{func} not supported")
+
+        return ref, out_enc
+
+    def get_errors(self):
+        """Computes the total error of approximations"""
+        abs_errors, relative_errors = [], []
+
+        for func in FuncBenchmarks.UNARY:
+            ref, out_enc = self.call_function_on_domain(func)
+            out = out_enc.get_plain_text()
+
+            abs_error = FuncBenchmarks.calc_abs_error(ref, out)
+            abs_errors.append(abs_error)
+
+            relative_error = FuncBenchmarks.calc_relative_error(ref, out)
+            relative_errors.append(relative_error)
+
+        return abs_errors, relative_errors
+
+    def run(self):
+        """Runs and stores benchmarks in self.df"""
+        _runtimes, runtimes_enc = self.get_runtimes()
+
+        abs_errors, relative_errors = self.get_errors()
+
+        self.df = pd.DataFrame.from_dict(
+            {
+                "function": FuncBenchmarks.UNARY,
+                "runtime": [r.mid for r in runtimes_enc],
+                "runtime Q1": [r.q1 for r in runtimes_enc],
+                "runtime Q3": [r.q3 for r in runtimes_enc],
+                "total abs err.": abs_errors,
+                "avg relative err.": relative_errors,
+            }
+        )
+
+
+def run_benches(tensor_size):
+    crypten.init()
+
+    device=torch.device("cpu")
+
+    logging.info("Tensor size '{}'".format(tensor_size))
+
+    benches = FuncBenchmarks(tensor_size, device=device)
+    benches.run()
+
+    print(benches)
---
+++ examples/benches/launcher.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+"""
+python examples/benches/launcher.py --world_size 2 --tensor_size 1000,10 --multiprocess
+"""
+
+import argparse
+import logging
+import os
+
+from examples.multiprocess_launcher import MultiProcessLauncher
+
+
+def get_args():
+    def tuple_type(s):
+        try:
+            # Split the string into integers
+            elements = tuple(map(int, s.split(',')))
+            return elements
+        except ValueError:
+            # Raise an error if parsing fails
+            raise argparse.ArgumentTypeError("Tuple format must be integers separated by commas")
+
+
+    parser = argparse.ArgumentParser(description="CrypTen Cifar Training")
+    parser.add_argument(
+        "--world_size",
+        type=int,
+        default=2,
+        help="The number of parties to launch. Each party acts as its own process",
+    )
+    parser.add_argument(
+        "-s",
+        "--tensor_size",
+        type=tuple_type,
+        default=(100, 100),
+        help="The size of the tensors as a tuple of integers separated by commas (e.g., '100,100,50')",
+    )
+    parser.add_argument(
+        "--multiprocess",
+        default=False,
+        action="store_true",
+        help="Run example in multiprocess mode",
+    )
+    args = parser.parse_args()
+    return args
+
+def _run_experiment(args):
+    # only import here to initialize crypten within the subprocesses
+    from examples.benches.benches import run_benches
+
+    # Only Rank 0 will display logs.
+    level = logging.INFO
+    if "RANK" in os.environ and os.environ["RANK"] != "0":
+        level = logging.CRITICAL
+    logging.getLogger().setLevel(level)
+
+    run_benches(args.tensor_size)
+
+def main(run_experiment):
+    args = get_args()
+
+    if args.multiprocess:
+        launcher = MultiProcessLauncher(args.world_size, run_experiment, args)
+        launcher.start()
+        launcher.join()
+        launcher.terminate()
+    else:
+        run_experiment(args)
+
+
+if __name__ == "__main__":
+    main(_run_experiment)
