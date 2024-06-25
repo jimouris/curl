ee616766dd346c132881de2799c823037a347284
manel1874 <manel1874@gmail.com>
2024-06-25 11:42:12 +0100

    fix: add multi-gpu flag and update MultiProcessLauncher inputs for benches folder

--- examples/benches/launcher.py
+++ examples/benches/launcher.py
@@ -83,6 +83,14 @@ def get_args():
         default="cpu",
         help="the device to run the benchmarks",
     )
+    parser.add_argument(
+        "--multi-gpu",
+        "-mg",
+        required=False,
+        default=False,
+        action="store_true",
+        help="use different gpu for each party. Will override --device if selected",
+    )
     args = parser.parse_args()
     return args
 
@@ -124,8 +132,7 @@ def main(run_experiment):
         raise ValueError("Communication statistics are not available for TTP provider")
 
     if args.multiprocess:
-        device = torch.device(args.device)
-        launcher = MultiProcessLauncher(args.world_size, run_experiment, args, cfg_file, device)
+        launcher = MultiProcessLauncher(args.world_size, run_experiment, args, cfg_file)
         launcher.start()
         launcher.join()
         launcher.terminate()
